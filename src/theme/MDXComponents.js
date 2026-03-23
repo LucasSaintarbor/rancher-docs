2b569d2451eacd40b06acbc39c2623cc2bdf70c2
Lucas Saintarbor <lucas.saintarbor@suse.com>
2026-03-23 10:25:30 -0700

    Update src/theme/MDXComponents.js
    Co-authored-by: Billy Tat <btat@suse.com>

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -16,7 +16,7 @@ import ConfigureSLOOidc from '/shared-files/_configure-slo-oidc.md';
 import EOLRKE1Warning from '/shared-files/_eol-rke1-warning.md';
 import PermissionsWarning from '/shared-files/_permissions-warning.md';
 import SamlOpenLDAPGroupPermissions from '/shared-files/_saml-openldap-group-permissions.md';
-import v3APITokentsDepecationWarning from '/shared-files/_v3-api-tokens-deprecation-warning.md';
+import v3APITokensDeprecationWarning from '/shared-files/_v3-api-tokens-deprecation-warning.md';
 
 export default {
   // Re-use the default mapping
